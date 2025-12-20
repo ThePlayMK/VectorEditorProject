99be737a1746ac9bef90be03ec1b92beab071f26
ThePlayMK <theplaymk@gmail.com>
2025-12-21 00:43:17 +0100

    Selfmade v.1

--- VectorEditor.UI/MainWindow.axaml.cs
+++ VectorEditor.UI/MainWindow.axaml.cs
@@ -1,17 +1,50 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Styling;
 
 namespace VectorEditor.UI
 {
     public partial class MainWindow : Window
     {
+        private Button? _activeToolButton;
+        private string _selectedColor="#000000";
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        // Toggles the hamburger menu
-        private void ToggleMenu(object sender, RoutedEventArgs e)
+        void ToggleThemeChange(object? sender, RoutedEventArgs? e)
+        {
+            if (ActualThemeVariant == ThemeVariant.Dark)
+            {
+                RequestedThemeVariant = ThemeVariant.Light;
+            }
+            else
+            {
+                RequestedThemeVariant = ThemeVariant.Dark;
+            }
+        }
+
+        void SelectTool(object? sender, RoutedEventArgs? e)
+        {
+            var clickedButton = sender as Button;
+            if (clickedButton == null) return;
+            if (_activeToolButton != null)
+            {
+                _activeToolButton.Classes.Remove("Selected");
+            }
+            clickedButton.Classes.Add("Selected");
+            _activeToolButton = clickedButton;
+        }
+
+        void UpdateColor(string color)
+        {
+            _selectedColor = color;
+            SelectedColor.Background = Brush.Parse(color);
+        }
+        
+        void ToggleMenu(object? sender, RoutedEventArgs? e)
         {
             MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;
         }
